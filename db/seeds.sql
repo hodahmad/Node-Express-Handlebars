622403904a287454356ab5807ce55c9317c6b745
Hoda Ahmad <ccs7vp@github.com>
2019-11-18 22:17:05 -0500

    Added files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Double Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Spicy Chicken Burger', false);
